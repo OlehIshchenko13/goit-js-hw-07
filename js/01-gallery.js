2ff16bf186cac8e35a154f48ab3fb280380d018c
Oleh <97728959+OlehIshchenko13@users.noreply.github.com>
2022-09-04 14:49:12 +0300

    push add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,56 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContRef = document.querySelector('.gallery')
+
+
+const createGalleryImgItem = ({preview,original,description}) => `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`;
+const galleryMarkup = galleryItems.reduce((acc,item)=>acc + createGalleryImgItem(item),'');
+
+galleryContRef.insertAdjacentHTML('afterbegin',galleryMarkup);
+
+
+
+
+galleryContRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+	e.preventDefault();
+ const galleryTarget = e.target.classList.contains("gallery__image")
+	if (!galleryTarget) {
+		return;
+	}
+
+	addOriginalImgToModal(e.target.dataset.source);
+}
+
+function addOriginalImgToModal(originalImageLink) {
+	const instance = basicLightbox.create(`
+    <img src="${originalImageLink}" width="900" height="700">
+`)
+
+	instance.show();
+
+	closeKeybord(instance);
+
+}
+
+function closeKeybord(instance) {
+	document.addEventListener("keydown", (e) => {
+		if (e.code == "Escape") {
+			instance.close()
+		}
+	}, { once: true });
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContRef = document.querySelector('.gallery')
+
+
+const createGalleryImgItem = ({preview,original,description}) => `<a class="gallery__item" href="${original}"}> 
+<img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+const galleryMarkup = galleryItems.reduce((acc,item)=>acc + createGalleryImgItem(item),'');
+
+galleryContRef.insertAdjacentHTML('afterbegin',galleryMarkup);
+
+
+const sliderLightBox = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 300,
+});
